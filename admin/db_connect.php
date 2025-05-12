0205126a72897805f3fa5c58d81d87a0585cc9eb
Thrisha Mahesh <120312633+thrishamahesh@users.noreply.github.com>
2025-05-12 23:59:11 +0530

    Update db_connect.php

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,3 +1,8 @@
 <?php 
+$db_host = getenv('DB_HOST');  // safer with env vars
+$db_user = getenv('DB_USER');
+$db_pass = getenv('DB_PASS');
+$db_name = getenv('DB_NAME');
 
-$conn= new mysqli('db','user','userpass','event_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name)
+    or die("Could not connect to mysql: " . mysqli_connect_error());
